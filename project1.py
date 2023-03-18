acf321de9cc26377b7f94607a1f5e31ec6cc2cea
bernadettemm <bernadettemassawe@gmail.com>
2023-03-18 16:44:49 +0300

    project1

--- project1.py
+++ project1.py
@@ -6,3 +6,12 @@ movie_dict = {'years':years, 'duration': durations}
 
 # Print the dictionary
 print(movie_dict)
+
+# Import pandas under its usual alias
+import pandas as pd
+
+# Create a DataFrame from the dictionary
+durations_df = pd.DataFrame(movie_dict)
+
+# Print the DataFrame
+print(durations_df) 
